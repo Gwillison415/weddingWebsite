e9cbce1f87da6521a8926c6fb8f88c43e52fdd50
Grant Willison <emailgdw@gmail.com>
2018-11-25 21:00:49 -0800

    update production pg connection HOST

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -1,23 +1,25 @@
 require('dotenv').config();
+
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/weddingdb',
+    connection: 'postgres://localhost/weddingdb'
   },
 
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/weddingdb',
+    connection: 'postgres://localhost/weddingdb'
   },
 
   production: {
     client: 'pg',
     connection: {
-       port: process.env.DB_PORT,
-       host: process.env.DB_HOST,
-    database: process.env.DATABASE_NAME,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_ACCESS_KEY,}
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_ACCESS_KEY
+      database: process.env.DATABASE_NAME,
+    }
 
-  },
+  }
 };
