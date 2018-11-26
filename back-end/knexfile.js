42b79b3a5d260168f386557b0afffb6d8974d5d8
Grant Willison <emailgdw@gmail.com>
2018-11-25 17:52:05 -0800

    update production knexfile

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -13,5 +13,9 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_ACCESS_KEY,
+    
   },
 };
