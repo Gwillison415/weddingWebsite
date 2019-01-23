1e298301d5b959e53915f27ec5bf2dc082371313
Grant Willison <emailgdw@gmail.com>
2019-01-23 09:59:48 -0800

    try https in origin

--- back-end/app.js
+++ back-end/app.js
@@ -52,7 +52,7 @@ if (process.env.NODE_ENV !== 'production') {
 // app.use(allowCrossDomain);
 
 var corsOptions = {
-  origin: 'http://www.sillywilliwedding.com',
+  origin: 'https://www.sillywilliwedding.com',
   // origin: 'http://localhost:3005',
   credentials: true,
 };
